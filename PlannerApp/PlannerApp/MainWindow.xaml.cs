64aa89b6f520b75c63f24e3ff72c4907e7555cc6
Michał Słowiński <62020662+mslowin@users.noreply.github.com>
2022-04-05 23:38:10 +0200

    Dodano nowe okna i wstępną obsługę bazy danych
    Dodano okno tworzenia nowego profilu i tworzenia nowego tasku (Na razie oba są puste)
    Okno nowego tasku pojawia się po naciśnięciu przycisku "dodaj zadanie" na oknie MainWindow
    Dodatkowo na konsoli "Dane wyjściowe" pojawiają się zawartości bazy danych Profiles (Na te chwile sa to imiona i nazwiska autorów)

--- PlannerApp/PlannerApp/MainWindow.xaml.cs
+++ PlannerApp/PlannerApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;                  //do wpisywania rzeczy do bazy danych
 
 namespace PlannerApp
 {
@@ -23,11 +24,27 @@ namespace PlannerApp
         public MainWindow()
         {
             InitializeComponent();
+
+            PlanerEntities profiles_db = new PlanerEntities();
+
+            var profiles = from d in profiles_db.Profiles
+                           select d;
+
+            foreach (var item in profiles)
+            {
+                Console.WriteLine(item.profile_id);
+                Console.WriteLine(item.profile_name);
+                Console.WriteLine(item.profile_surname);
+                Console.WriteLine(item.profile_date_of_birth);
+            }    
         }
 
         private void DodajZadanie_Click(object sender, RoutedEventArgs e)
         {
             Text1.Text = "Nowe zadanie";
+
+            NewTaskWindow window2 = new NewTaskWindow();
+            window2.Show();
         }
     }
 }
